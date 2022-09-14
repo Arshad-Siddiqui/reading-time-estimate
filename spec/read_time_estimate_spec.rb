43baf65ae809d2347f5c5aed39ed075f6303993f
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-14 12:56:29 +0100

    Fix indentation in the tests

--- spec/read_time_estimate_spec.rb
+++ spec/read_time_estimate_spec.rb
@@ -2,9 +2,9 @@ require "read_time_estimate"
 
 RSpec.describe '#read_time_estimate' do
   context 'More than 0 words are given' do
-  it 'converts 200 to 1' do
-    result = read_time_estimate(200)
-    expect(result).to eq 1
-  end
+    it 'converts 200 to 1' do
+      result = read_time_estimate(200)
+      expect(result).to eq 1
+    end
   end
 end
