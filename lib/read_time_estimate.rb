a64d3faa483257dc01a1573905d9c4bfcf5ee8c5
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-14 13:19:00 +0100

    Fix said typo where else

--- lib/read_time_estimate.rb
+++ lib/read_time_estimate.rb
@@ -1,4 +1,4 @@
 def read_time_estimate(word_count)
-  if word_count == nil then fail 'Expected number, received null' end
+  if word_count == nil then fail 'Expected number, received nill' end
   word_count.to_f / 200
 end
